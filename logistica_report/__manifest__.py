c3001604f3c31a872f48725b572627710539f016
AdsOdoo <60229928+AdsOdoo@users.noreply.github.com>
2020-08-10 12:43:14 -0500

    Logística Report folio

---
+++ logistica_report/__manifest__.py
@@ -0,0 +1,22 @@
+
+
+{
+    'name': 'Reporte de Logistica',
+    'version': '13.1',
+    'description': ''' Agrega un reporte de logistica
+    ''',
+    'category': 'project',
+    'author': 'Sergio Medrano',
+    'website': 'www.saludyhogar.mx',
+    'depends': [
+        'base','project',
+    ],
+    'data': [
+        'report/report_folio_logistica.xml',
+    ],
+    'application': False,
+    'installable': True,
+    'price': 0.00,
+    'currency': 'USD',
+    'license': 'OPL-1',	
+}
